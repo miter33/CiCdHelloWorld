0f85ee4c6ed79d70f65e54d7910d4b9d9473d9de
Aliaksei Huivan <a.huivan@godeltech.com>
2021-12-25 16:16:33 +0300

    add test with error

---
+++ CiCdHelloWorld.Tests/ProgramTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CiCdHelloWorld.Tests
+{
+    public class ProgramTest
+    {
+        [Fact]
+        public void MainTest()
+        {
+            var result = false;
+            Assert.False(result);
+        }
+    }
+}
